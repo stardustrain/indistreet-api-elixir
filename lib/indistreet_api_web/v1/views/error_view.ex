40fa7c60eb573d31aeb7b68c7c283976fc1708ac
stardustrain <kthanterran@gmail.com>
2022-03-17 14:44:20 +0900

    test: fix broken tests

---
+++ lib/indistreet_api_web/v1/views/error_view.ex
@@ -0,0 +1,16 @@
+defmodule IndistreetApiWeb.V1.ErrorView do
+  use IndistreetApiWeb, :view
+
+  # If you want to customize a particular status code
+  # for a certain format, you may uncomment below.
+  # def render("404.json", _assigns) do
+  #   "Wow"
+  # end
+
+  # By default, Phoenix returns the status message from
+  # the template name. For example, "404.html" becomes
+  # "Not Found".
+  def template_not_found(template, _assigns) do
+    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+end
