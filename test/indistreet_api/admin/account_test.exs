58a378cab619a640a49ffa7d6063a3de47799321
stardustrain <kthanterran@gmail.com>
2022-04-04 16:49:48 +0900

    feat: add admin account repository

---
+++ test/indistreet_api/admin/account_test.exs
@@ -0,0 +1,24 @@
+defmodule IndistreetApi.Admin.AccountTest do
+  use IndistreetApi.DataCase
+  
+  alias IndistreetApi.Admin.Account
+  import IndistreetApi.UserFixture
+
+  describe "users" do
+    alias IndistreetApi.Model.Account.User
+
+    test "toggle_user_authorization/1 toggle is_admin flag for user" do
+      %{user: user} = create_user()
+
+      {:ok, user} = Account.toggle_user_authorization(user)
+      assert user.is_admin === true
+      {:ok, user} = Account.toggle_user_authorization(user)
+      assert user.is_admin === false
+    end
+  end
+
+  defp create_user() do
+    user = user_fixture()
+    %{user: user}
+  end
+end
